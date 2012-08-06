17fa4fa5194c1cff5d40e3647465a16e74ae9b71
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2012-08-06 13:54:46 -0700

    fix path

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export TOOLCH=toolchain
-export PATH=$TOOLCH:$PATH
+export PATH=$TOOLCH/bin:$PATH
 
 $NDKPATH/build/tools/make-standalone-toolchain.sh --platform=android-9 --install-dir=$TOOLCH
