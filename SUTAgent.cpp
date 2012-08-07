f92e0b5ce5da963d4d7bcfdc206ae738043b7d81
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2012-08-07 13:22:31 -0700

    start working on basic functions

--- SUTAgent.cpp
+++ SUTAgent.cpp
@@ -7,24 +7,57 @@
 #include <iostream>
 #include <vector>
 
-//using namespace std;
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+
+#define BUFSIZE 1024
+
+
+FILE *checkPopen(std::string cmd, std::string mode) {
+  FILE *fp = popen(cmd.c_str(), mode.c_str());
+  if (!fp) {
+    fprintf(stderr, "Error on popen: %s, with mode %s.\n", cmd.c_str(), mode.c_str());
+    exit(1);
+  }
+  return fp;
+}
+
+bool cd(std::string path) {
+  int success = chdir(path.c_str());
+  return (success == 1);
+}
+
+std::string cwd() {
+  char output[BUFSIZE];
+  getcwd(output, BUFSIZE);
+
+  return std::string(output);
+}
+
+uint64_t clok() {
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  return (uint64_t) ts.tv_sec * 1000 + ts.tv_nsec / 1000000.0;
+}
 
 int main(int argc, char **argv) {
 
-  std::cout << "Hello, world!" << std::endl;
+  char output[BUFSIZE];
 
-  std::vector<int> ints;
-  int i;
-  for (i = 0; i < 25; ++i) {
-    ints.push_back(i);
+  FILE *uname = checkPopen("uname -s -m -r", "r");
+  while (fgets(output, BUFSIZE, uname)) {
+    puts(output);
   }
 
-  while (!ints.empty()) {
-    std::cout << ints.back() << " ";
-    ints.pop_back();
-  }
+  cd("/data/local");
+  std::cout << cwd() << std::endl;
 
-  std::cout << std::endl;
+  std::cout << clok() << std::endl;
+  std::cout << time(NULL) << std::endl;
 
   return 0;
 }
