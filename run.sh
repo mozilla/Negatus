9acefc15a111c09921b924145ad753956c5454cd
Mark Côté <mcote@alumni.uwaterloo.ca>
2012-08-20 17:02:20 -0400

    Fixed run.sh.

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+rm agent
+make -f Makefile.droid
 $ADB shell "rm /data/local/agent"
 $ADB shell "rm -rf /data/local/tests"
 $ADB shell "rm -rf /data/local/tmp*"
 $ADB push agent /data/local
-$ADB push nspr/libnspr4.so /data/local
-$ADB push nspr/libplc4.so /data/local
-$ADB push nspr/libplds4.so /data/local
-$ADB shell "cd /data/local; LD_LIBRARY_PATH=. ./agent"
+$ADB push libs/libnspr4.so /data/local
+$ADB push libs/libplc4.so /data/local
+$ADB push libs/libplds4.so /data/local
+$ADB shell "LD_LIBRARY_PATH=/data/local /data/local/agent"
