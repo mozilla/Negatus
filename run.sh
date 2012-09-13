5b50ae6c6757194aa23b5eba84b939ab2e828141
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2012-09-13 16:50:46 -0700

    s/ADB/ADB_PATH to be consistent with the other codebases

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 rm agent
 make -f Makefile.droid
-$ADB shell "rm /data/local/agent"
-$ADB shell "rm -rf /data/local/tests"
-$ADB shell "rm -rf /data/local/tmp*"
-$ADB push agent /data/local
-$ADB push libs/libnspr4.so /data/local
-$ADB push libs/libplc4.so /data/local
-$ADB push libs/libplds4.so /data/local
-$ADB shell 'export NSPR_LOG_MODULES="NegatusLOG:5, timestamp" \
+$ADB_PATH shell "rm /data/local/agent"
+$ADB_PATH shell "rm -rf /data/local/tests"
+$ADB_PATH shell "rm -rf /data/local/tmp*"
+$ADB_PATH push agent /data/local
+$ADB_PATH push libs/libnspr4.so /data/local
+$ADB_PATH push libs/libplc4.so /data/local
+$ADB_PATH push libs/libplds4.so /data/local
+$ADB_PATH shell 'export NSPR_LOG_MODULES="NegatusLOG:5, timestamp" \
   && LD_LIBRARY_PATH=/data/local /data/local/agent'
