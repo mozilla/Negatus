4f49e67adb5bb75657588e24b62d615880a8013c
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2012-09-14 16:52:32 -0700

    s/ADB/ADB_PATH in setup-tools

--- setup-tools.sh
+++ setup-tools.sh
@@ -4,7 +4,7 @@ if [ ! -f busybox-armv6l ]
 then
     wget http://busybox.net/downloads/binaries/1.19.0/busybox-armv6l
 fi
-$ADB remount
-$ADB push busybox-armv6l /system/bin/busybox
+$ADB_PATH remount
+$ADB_PATH push busybox-armv6l /system/bin/busybox
 
-$ADB shell 'cd /system/bin; chmod 555 busybox; for x in `./busybox --list`; do ln -s ./busybox $x; done'
+$ADB_PATH shell 'cd /system/bin; chmod 555 busybox; for x in `./busybox --list`; do ln -s ./busybox $x; done'
