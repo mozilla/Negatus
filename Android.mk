f2382e4c00cd76eb6703661346ad129a1de1ef51
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2012-09-18 14:20:06 -0700

    update src files for android.mk

--- Android.mk
+++ Android.mk
@@ -5,12 +5,21 @@ LOCAL_SRC_FILES:= \
 	src/Buffer.cpp \
 	src/BufferedSocket.cpp \
 	src/CommandEventHandler.cpp \
+	src/EventHandler.cpp \
+	src/Hash.cpp \
+	src/HeartbeatEventHandler.cpp \
+	src/Logger.cpp \
 	src/Logging.cpp \
+	src/PullFileEventHandler.cpp \
+	src/PushFileEventHandler.cpp \
 	src/Reactor.cpp \
-	src/SessionEventHandler.cpp \
+	src/Registration.cpp \
+	src/Shell.cpp \
 	src/SocketAcceptor.cpp \
 	src/Strings.cpp \
-	src/SUTAgent.cpp
+	src/SUTAgent.cpp \
+	src/Subprocess.cpp \
+	src/SubprocessEventHandler.cpp
 
 LOCAL_LDFLAGS += -L$(LOCAL_PATH)/libs -lplds4 -lplc4 -ldl -lnspr4
 LOCAL_SHARED_LIBRARIES := libstlport
